0f1bc659f713304b70715ed21c446eecce2ec649
Sarnav07 <sarnavkansal81@gmail.com>
2026-03-14 07:30:02 +0000

    Updated Backend

--- Project/Backend/src/config/db.js
+++ Project/Backend/src/config/db.js
@@ -5,18 +5,19 @@ const serviceAccount = JSON.parse(
     await readFile(new URL("../../firebase-key.json",import.meta.url))
 );
 
-export const connectDB= () => {
+export const connectDB = () => {
     try {
-        admin.initializeApp({
-            credential:admin.credential.cert(serviceAccount),
-        });
-        console.log("firebase connected successfully!");
+        if (admin.apps.length === 0) {
+            admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount),
+            });
+            console.log("firebase connected successfully!");
+        }
         return admin.firestore();
-    }
-    catch (error) {
-        console.error("firebase is not able to connect",error);
+    } catch (error) {
+        console.error("firebase is not able to connect", error);
         process.exit(1);
     }
 };
 
-export const db = admin.apps.length ? admin.firestore() : connectDB();
+export const db = connectDB();
